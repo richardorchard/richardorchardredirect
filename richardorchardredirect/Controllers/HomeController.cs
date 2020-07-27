d467ce45a858907ad05a6706e339d7d4f76de3b3
Richard Orchard <me@richardorchard.com>
2020-07-27 09:58:57 +0100

    setting up simple redirect website

---
+++ richardorchardredirect/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using richardorchardredirect.Models;
+
+namespace richardorchardredirect.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            return Redirect("http://www.richardorchard.com/");
+        }
+
+      
+    }
+}
